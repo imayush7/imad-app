f37fbc9b929120c4e1532ab206dedf8f558fb0c8
Ayush Singh <ayushbsp98@gmail.com>
2017-08-19 22:18:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,22 @@
-console.log('Loaded!');
-//move the image
+//Counter code
+var button = document.getElementById('counter');
 
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-var img = document.getElementById('madi');
-img.onclick = function(){
-    var interval = setInterval(moveRight,100);
+button.onclick = function() {
+    //create a new request
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take action
+            if(request.status===200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    //Make the request
+    request.open('GET','http://ayushbsp98.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
