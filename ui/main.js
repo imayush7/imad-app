c3a02297d6f65e5823deddfb925ec5772c602943
Ayush Singh <ayushbsp98@gmail.com>
2017-08-19 20:12:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,13 @@
 console.log('Loaded!');
 //move the image
+
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 var img = document.getElementById('madi');
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,100);
 };
