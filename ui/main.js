e181befd08c16109909e06848d13aeb801238e93
Ayush Singh <ayushbsp98@gmail.com>
2017-08-20 14:53:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,6 @@ button.onclick = function() {
         }
     };
     //Make the request
-    request.open('GET','http://ayushbsp98.imad.hasura-app.io/counter',true);
+    request.open("GET",'http://ayushbsp98.imad.hasura-app.io/counter',true);
     request.send(null);
 };
