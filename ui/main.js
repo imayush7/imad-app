77e71fd24055bab5077947f19e13d49396e5d790
Ayush Singh <ayushbsp98@gmail.com>
2017-08-20 15:01:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,5 +18,5 @@ button.onclick = function() {
     };
     //Make the request
     request.open('GET','http://ayushbsp98.imad.hasura-app.io/counter',true);
-    request.send(null);
+    //request.send(null);
 };
