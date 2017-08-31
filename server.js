8a3ac651a259b111fc9f97348f2dd08f4613f64b
Ayush Singh <ayushbsp98@gmail.com>
2017-08-31 21:19:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config = {
+    user = 'ayushbsp98',
+    db = 'ayushbsp98',
+    host = 'db.imad.hasura-app.io',
+    port = '5432',
+    password = 'process.env.DB_PASSWORD';
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -43,9 +52,18 @@ var articles ={
 }
 };
 
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
     //return a response
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    })
 });
 
 function createTemplate(data){
